c9ff7b5cf4e633dfe31d8708409549d0c76b90ef
yi4534 <yihui86.huang@gmail.com>
2025-09-02 14:47:34 +0800

    feat(generator): 为 API 文档添加默认值
    - 在 API 文档注释中添加了 @Summary、@Description、@Accept 和 @Produce 的默认值
    -这些默认值可以帮助开发者更快速地创建和维护 API 文档

--- cmd/hz/generator/handler.go
+++ cmd/hz/generator/handler.go
@@ -296,6 +296,10 @@ func (m *HttpMethod) InitComment() {
 	} else {
 		text = "// " + m.Name + " " + text
 	}
+	text += "// @Summary 测试 Summary\n"
+	text += "// @Description 测试 Summary\n"
+	text += "// @Accept application/json\n"
+	text += "// @Produce application/json\n"
 	text = strings.Replace(text, "\n", "\n// ", -1)
 	if !strings.Contains(text, "@router ") {
 		text += "\n// @router " + m.Path
